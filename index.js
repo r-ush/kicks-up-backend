87326de7d3551eedea85df0ba081fdb98319cd90
r-ush <aarush.bhatt@gmail.com>
2020-05-04 21:14:37 +0530

    init index.js

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express=require('express')
+const userRouter=require('./src/routers/user.router')
+const productRouter=require('./src/routers/product.router')
  
 const app = express();
 const port= process.env.PORT || 3001
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
-  });
+	res.json({
+		message: 'Hello World!'
+	})
+});
+
+app.use(express.json())
+app.use('/users',userRouter)
+app.use('/products',productRouter)
  
 app.listen(port, () =>
-  console.log(`App listening on port ${port} !`),
+	console.log(`App listening on port ${port} !`),
 );
