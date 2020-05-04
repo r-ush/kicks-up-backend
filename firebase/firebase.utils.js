d4b37eaf70ab0e366e31bc8865e086fc92c7749b
r-ush <aarush.bhatt@gmail.com>
2020-05-05 01:43:08 +0530

    init firebase

--- firebase/firebase.utils.js
+++ firebase/firebase.utils.js
@@ -1,5 +1,8 @@
-// var firebase = require('firebase/app');
-// require('firebase/auth');
-// require('firebase/database');
- 
-// var app = firebase.initializeApp({ ... });
+var admin = require('firebase-admin');
+
+var serviceAccount = require("/home/rush/Downloads/kicks-up-firebase-adminsdk-rihcz-c1a6afb1d3.json");
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://kicks-up.firebaseio.com"
+  });
